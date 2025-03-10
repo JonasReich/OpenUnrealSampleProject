9266cabefa89b9e0fcb8b4b84c580419813cf606
Jonas Reich <mail@jonasreich.de>
2025-03-10 16:55:39 +0100

    Adjusted update script to also update OUUTags from TQ2

--- UpdateFromTQ2.cmd
+++ UpdateFromTQ2.cmd
@@ -15,17 +15,29 @@ IF %ERRORLEVEL%==1 ( EXIT /B 1 )
 CALL :UPDATE_SUBMODULE Plugins\OUUJsonDataAssets p4/tq2-code
 IF %ERRORLEVEL%==1 ( EXIT /B 1 )
 
+CALL :UPDATE_SUBMODULE Plugins\OUUTags p4/tq2-code
+IF %ERRORLEVEL%==1 ( EXIT /B 1 )
+
 CALL :UPDATE_SUBMODULE OpenUnrealAutomationTools p4/tq2-main
 
 EXIT /B %ERRORLEVEL%
 
 
 :UPDATE_SUBMODULE
-REM change working directory to first param of subroutine
-PUSHD %~dp0%1
 
+SET "REPO_RELATIVE_PATH=%1"
 SET "GIT_BRANCH=%2"
 SET "GIT_BRANCH_BACKUP=%GIT_BRANCH%-backup"
+
+ECHO --------------------------------------------------------------
+ECHO UPDATING SUBMODULE %REPO_RELATIVE_PATH%
+ECHO --------------------------------------------------------------
+
+PUSHD %~dp0%REPO_RELATIVE_PATH%
+
+REM weird bash stuff: get name of the repo directory
+for %%F in ("%REPO_RELATIVE_PATH%") do set "REPO_NAME=%%~nxF"
+ECHO REPO_NAME = %REPO_NAME%
 
 git config --replace-all user.name "Jonas Reich"
 REM all updates originating from TQ2 should be signed by my Grimlore work mail
@@ -34,7 +46,8 @@ git config --replace-all user.email jreich@grimloregames.com
 git checkout %GIT_BRANCH%
 git checkout -b %GIT_BRANCH_BACKUP%
 
-git p4j %GIT_BRANCH%
+ECHO git p4j %GIT_BRANCH% --repo-name %REPO_NAME%
+git p4j %GIT_BRANCH% --repo-name %REPO_NAME%
 SET P4J_STATUS=%ERRORLEVEL%
 
 git checkout %GIT_BRANCH%
